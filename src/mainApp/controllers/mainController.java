f0c30defff14e577defb3814d8250effe2629e9b
albinwarvelin <albin.warvelin@gmail.com>
2021-07-10 22:51:17 +0200

    Added Webview for Strava Authorization
    Added Webview for strava authorization, displays strava oauth link in app.
    Added handles for all buttons
    Added css for datepicker
    Added logout button
    Added settings button
    Added refreshbutton
    
    Included fontawesome library (not neccessary in final version)

--- src/mainApp/controllers/mainController.java
+++ src/mainApp/controllers/mainController.java
@@ -1,6 +1,73 @@
 package mainApp.controllers;
 
-public class mainController
-{
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainController implements Initializable
+{
+    /* Scene */
+    public StackPane contentSP;
+    public WebView authorizationWV;
+
+
+
+    /* Run on first startup */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
+    }
+
+    private void handleAuthorization()
+    {
+        openAuthorization();
+    }
+
+    private void openAuthorization()
+    {
+        authorizationWV.getEngine().load("https://www.strava.com/oauth/authorize?client_id=67536&response_type=code&redirect_uri=http://localhost&approval_prompt=force&scope=read_all,activity:read_all");
+    }
+
+    /* Handles buttons */
+    public void authorizationButton_Action()
+    {
+        handleAuthorization();
+    }
+    public void attributesButton_Action()
+    {
+
+    }
+
+    public void graphsButton_Action()
+    {
+
+    }
+
+    public void logoButton_Action()
+    {
+
+    }
+
+    public void settingsButton_Action()
+    {
+
+    }
+
+    public void refreshButton_Action()
+    {
+
+    }
+
+    public void signOutButton_Action()
+    {
+
+    }
 }
